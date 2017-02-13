c0ba2f0a1040c0013e645e31da2f2ee1b5a9bd9f
lucas <lucasjgordon@gmail.com>
2017-02-13 19:06:39 +0000

    Adds ability to read multiple files.

--- Cat.java
+++ Cat.java
@@ -1,28 +1,22 @@
 import java.io.*;
+import java.util.Arrays;
 
 public class Cat {
 	public static void main(String[] args) {
 		try {
-			String targetFileName = args[0];
 			File f = new File("/Users/lucasgordon/Desktop/week-sixteen/");
 			File[] files = f.listFiles();
-			File file = null;
 			for (int i = 0; i < files.length; i++) {
-				if (!files[i].getName().equals(targetFileName)) {
+				if (Arrays.asList(args).indexOf(files[i].getName()) == -1) {
 					continue;
 				}
-				file = files[i];
-				break;
-			}
-			if (file == null) {
-				System.out.println("File not found.");
-				return;
-			}
-			BufferedReader in = new BufferedReader(new FileReader(file));
-			String line = in.readLine();
-			while (line != null) {
-				System.out.println(line);
-				line = in.readLine();
+				BufferedReader in = new BufferedReader(new FileReader(files[i]));
+				String line = in.readLine();
+				while (line != null) {
+					System.out.println(line);
+					line = in.readLine();
+				}
+				System.out.println();
 			}
 		} catch(Exception e) {
 			e.printStackTrace();
